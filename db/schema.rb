e62b3b954b793861ba1a171c34ca7f965c563cb8
tessa setiadi <tessa.setiadi@gmail.com>
2014-06-29 17:10:55 +1000

    fixed pins

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140629060439) do
+ActiveRecord::Schema.define(version: 20140629065512) do
 
   create_table "pins", force: true do |t|
-    t.string   "descriptions"
+    t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
